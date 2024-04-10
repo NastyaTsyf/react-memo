9734b0a680c6226057a4612672d8de98ef52df1c
твое имя < >
2024-04-10 22:17:03 +0400

    дз №1

--- src/pages/GamePage/GamePage.jsx
+++ src/pages/GamePage/GamePage.jsx
@@ -4,10 +4,13 @@ import { Cards } from "../../components/Cards/Cards";
 
 export function GamePage() {
   const { pairsCount } = useParams();
+  const { isGameMode } = useParams();
+  console.log(isGameMode);
+  console.log(pairsCount);
 
   return (
     <>
-      <Cards pairsCount={parseInt(pairsCount, 10)} previewSeconds={5}></Cards>
+      <Cards pairsCount={parseInt(pairsCount, 10)} previewSeconds={5} isGameMode={isGameMode}></Cards>
     </>
   );
 }
