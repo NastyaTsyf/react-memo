1661ff94ef536665d8a464ab15f6d44a48b348ed
твое имя < >
2024-06-21 13:17:54 +0400

    1

--- src/lib.jsx
+++ src/lib.jsx
@@ -1,3 +1,15 @@
+export const superPowerData = {
+  alohomora: {
+    title: "Алохомора",
+    text: "Открывается случайная пара карт",
+  },
+};
+
+export const achievementsText = {
+  hardMode: "Игра пройдена в сложном режиме",
+  withoutSuperPowers: "Игра пройдена без супер-сил",
+};
+
 export const achievementsIcons = {
   hardMode: (
     <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
